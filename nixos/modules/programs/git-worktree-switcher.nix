98b563a5093a025ae2fcef0904383bd1451107f5
jiriks74 <jiri@stefka.eu>
2024-11-12 19:20:30 +0100

    nixos/git-worktree-switcher: init git-worktree-switcher This module sets up shells so that they work with [git-worktree-switcher](https://github.com/mateusauler/git-worktree-switcher)

---
+++ nixos/modules/programs/git-worktree-switcher.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  prg = config.programs;
+  cfg = prg.git-worktree-switcher;
+
+  bashInitScript = ''
+    eval "$(command ${pkgs.git-worktree-switcher}/bin/wt init bash)"
+  '';
+  zshInitScript = ''
+    eval "$(command ${pkgs.git-worktree-switcher}/bin/wt init zsh)"
+  '';
+  fishInitScript = ''
+    eval "$(command ${pkgs.git-worktree-switcher}/bin/wt init zsh)"
+  '';
+in
+{
+  options = {
+    programs.git-worktree-switcher = {
+      enable = lib.mkEnableOption "git-worktree-switcher, switch between git worktrees with speed.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ git-worktree-switcher ];
+
+    programs.bash.interactiveShellInit = bashInitScript;
+    programs.zsh.interactiveShellInit = lib.mkIf prg.zsh.enable zshInitScript;
+    programs.fish.interactiveShellInit = lib.mkIf prg.fish.enable fishInitScript;
+  };
+}
